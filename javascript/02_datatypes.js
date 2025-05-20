858bc99ba14abc6f842eca1a1c159e1c8574c06f
sumit kumar <panditsumit8825@gmail.com>
2025-05-20 15:47:07 +0530

    first changes

---
+++ javascript/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") we are using nodejs, not browser
+
+console.log(3+3)
+console.log("Sumit")
+
+let name="sumit"
+let age=34
+let isLoggedin=true
+
+
+// number => 2 to power 53
+// bigint
+// string=>""
+// boolean=true/false
+// null=>standolone value
+// undefined=>
+// symbol=> unique
+
+
+// object
+
+console.log(typeof("sumit"))
+console.log(typeof(null))
+console.log(typeof(undefined))
+
---
+++ javascript/test.js
@@ -0,0 +1,20 @@
+console.log("Sumit")
+console.log("Prince")
+const accountId = 144553
+let accountEmail= "sumitkr04@gmail.com"
+var accountPassword ="123456"
+accountCity="jaipur"
+
+// accountId =2 not allowed
+accountEmail= "sumit04@gmail.com"
+accountPassword ="1256"
+accountCity="jalandhar"
+let accountState
+console.log(accountId);
+
+
+/*
+prefer not to use var 
+because of issue in block   
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
