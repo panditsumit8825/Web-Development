d76a4e3521b6a299557b899cfca44efe67d94ab8
sumit kumar <panditsumit8825@gmail.com>
2025-05-31 01:00:40 +0530

    first changes

--- javascript/08_objects.js
+++ javascript/08_objects.js
@@ -4,34 +4,36 @@
 
 // Object.create
 
-const mySym = Symbol("Key1")
-const jsUser = {
-    name: "Sumit",
-    "full name": "Sumit kumar",
-    age: 19,
-    [mySym]:"myKey1",
-    location:"Noida",
-    email:"kr@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays:["Mondays","Saturdays"]
-}
-console.log(jsUser.email);
-console.log(jsUser["email"]);
+// const mySym = Symbol("Key1")
+// const jsUser = {
+//     name: "Sumit",
+//     "full name": "Sumit kumar",
+//     age: 19,
+//     [mySym]:"myKey1",
+//     location:"Noida",
+//     email:"kr@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays:["Mondays","Saturdays"]
+// }
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
 
-// console.log(jsUser.full name);
-console.log(jsUser["full name"]);
-console.log(jsUser[mySym]);
+// // console.log(jsUser.full name);
+// console.log(jsUser["full name"]);
+// console.log(jsUser[mySym]);
 
-jsUser.email="krr@gmail.com"
-console.log(jsUser["email"]);
+// jsUser.email="krr@gmail.com"
+// console.log(jsUser["email"]);
 
-jsUser.greeting = function(){
-    console.log("Hello Js User");
-}
+// jsUser.greeting = function(){
+//     console.log("Hello Js User");
+// }
 
-console.log(jsUser.greeting());
+// console.log(jsUser.greeting());
 
-jsUser.greetingTwo = function(){
-    console.log(`Hello Js User, ${this.name}`);
-}
-console.log(jsUser.greetingTwo())
+// jsUser.greetingTwo = function(){
+//     console.log(`Hello Js User, ${this.name}`);
+// }
+// console.log(jsUser.greetingTwo())
+
+// 
