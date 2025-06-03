31348ee34bf51a8750aff0b08da0a47d74c322d5
sumit kumar <panditsumit8825@gmail.com>
2025-06-03 22:37:19 +0530

    first changes

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,10 +1,36 @@
-console.log("s");
-console.log("u");
-function loginUserName(username){
-    if(username==undefined){
-        console.log("Please enter a username")
-    }
-    return `${username} just logged in`
+// console.log("s");
+// console.log("u");
+// function loginUserName(username){
+//     if(username==undefined){
+//         console.log("Please enter a username")
+//     }
+//     return `${username} just logged in`
+// }
+// // console.log(loginUserName("hitesh"))
+// console.log(loginUserName())
+
+function calculateCartPrice(val1, val2,...num1){
+    return num1
 }
-// console.log(loginUserName("hitesh"))
-console.log(loginUserName())
+
+console.log(calculateCartPrice(200,400,500,2000))
+const user = {
+    username: "hitesh",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+// handleObject(user)
+handleObject({
+    username:"Ram",
+    Price: 399
+})
+
+const myNewArray = [200,400,500,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray))
