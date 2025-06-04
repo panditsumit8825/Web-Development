412b5c61e728787c2e7c6ae2c0531a63d54eb173
sumit kumar <panditsumit8825@gmail.com>
2025-06-04 10:45:54 +0530

    first changes

--- javascript/scope.js
+++ javascript/scope.js
@@ -8,16 +8,43 @@
 
 // var c = 300
 
-let a = 300
-if(true){
-    let a = 10
-    const b = 20
-    // var c = 30
-    // c = 30
-    console.log("INNER:", a)
-}
+// let a = 300
+// if(true){
+//     let a = 10
+//     const b = 20
+//     // var c = 30
+//     // c = 30
+//     console.log("INNER:", a)
+// }
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// function one(){
+//     const username = "hitesh"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
+//     two()
+// }
+// one()
+
+// ********** interesting************
+
+addOne(5)
+console.log(addOne(5))
+function addOne(value){
+    return value + 1
+}
+// addOne(5)
+
+// addTwo(6)
+const addTwo = function(value){
+    return value + 2
+}
+// addTwo(6)
