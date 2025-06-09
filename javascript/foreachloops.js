0c225c170a2dd1a79fe34150c5a4e566d5362740
sumit kumar <panditsumit8825@gmail.com>
2025-06-10 01:04:55 +0530

    first changes

--- javascript/foreachloops.js
+++ javascript/foreachloops.js
@@ -1,4 +1,4 @@
-const codding = ['js','ruby','java','python','cpp']
+// const codding = ['js','ruby','java','python','cpp']
 
 // codding.forEach( function (item) {
 //     console.log(item);
@@ -18,21 +18,31 @@ const codding = ['js','ruby','java','python','cpp']
 //     console.log(item, index, arr);
 // })
 
-const myCoding = [
-    {
-        languageName: 'javascript',
-        laguagefile: 'js'
-    },
-     {
-        languageName: 'java',
-        laguagefile: 'java'
-    },
-     {
-        languageName: 'python',
-        laguagefile: 'py'
-    },
-]
+// const myCoding = [
+//     {
+//         languageName: 'javascript',
+//         laguagefile: 'js'
+//     },
+//      {
+//         languageName: 'java',
+//         laguagefile: 'java'
+//     },
+//      {
+//         languageName: 'python',
+//         laguagefile: 'py'
+//     },
+// ]
 
-myCoding.forEach( (item) => {
-    console.log(item.languageName);
+// myCoding.forEach( (item) => {
+//     console.log(item.languageName);
+// })
+
+
+//  Uses of Filter Method
+const myNums =[1,2,3,4,5,6,7,8,9,10]
+// const newNums = myNums.filter( (num) => num > 4)
+// console.log(newNums);
+const newNums = myNums.filter((num) => {
+    return num > 4;
 })
+console.log(newNums);
