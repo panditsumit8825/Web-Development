505e27220a03c1e22dae67d45e8fdb5626275dbf
sumit kumar <panditsumit8825@gmail.com>
2025-06-10 08:51:00 +0530

    first changes

--- javascript/foreachloops.js
+++ javascript/foreachloops.js
@@ -42,7 +42,21 @@
 const myNums =[1,2,3,4,5,6,7,8,9,10]
 // const newNums = myNums.filter( (num) => num > 4)
 // console.log(newNums);
-const newNums = myNums.filter((num) => {
-    return num > 4;
-})
+// const newNums = myNums.filter((num) => {
+//     return num > 4;
+// })
+
+// const newNums = []
+// myNums.forEach((num) => {
+//     if (num > 4) {
+//         newNums.push(num);
+//     }
+// });
+// console.log(newNums);
+
+
+const myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+// let newNums = myNumbers.map((num) => num +10)
+let newNums = myNumbers.map((num) => {return num +10})
 console.log(newNums);
